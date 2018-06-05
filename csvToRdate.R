ff0d3fe4ea2bc9d99a9ac5d9d6272a5cf8d46c5d
gayansandaruwan <gayankavirathne.14@cse.mrt.ac.lk>
2018-06-05 17:06:26 +0530

    Removing an unwanted file

--- csvToRdate.R
+++
@@ -1,6 +0,0 @@
-library("rio")
-
-srilanka <- import("/home/gayan/Desktop/CMLARE/Data/Processed/processed_file.csv")
-export(srilanka,"/home/gayan/Desktop/CMLARE/RAINLINK/data/sriLanka.RData")
-
-#import("/home/gayan/Desktop/CMLARE/RAINLINK/data/sriLanka.RData")
