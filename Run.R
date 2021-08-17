4bb561595e920dc290ef2f21f262431010f86dae
overeem11 <overeem@knmi.nl>
2021-08-17 16:13:38 +0200

    Add files via upload

--- Run.R
+++ Run.R
@@ -1,8 +1,8 @@
 ## The RAINLINK package. Retrieval algorithm for rainfall mapping from microwave links 
 ## in a cellular communication network.
 ##
-## Version 1.20
-## Copyright (C) 2020 Aart Overeem
+## Version 1.21
+## Copyright (C) 2021 Aart Overeem
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
